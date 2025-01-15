256054d0609b902017825623ff20513c0a410a2c
sk-0908 <105331038+sk-0908@users.noreply.github.com>
2025-01-16 02:13:15 +0900

    changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "w1980.blob.core.windows.net" },
